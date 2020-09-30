5b3c5365fdd1aa27766a781c471a8941bd2ca9c3
unknown <tusharsinha.dev@gmail.com>
2020-09-30 19:53:25 +0530

    keys changed

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == 'production') {
   //set production keys
-  module.exports = require('/prod');
+  module.exports = require('./prod');
 } else {
   //set dev keys
   module.exports = require('./dev');
